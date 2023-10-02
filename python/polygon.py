95ed99455982939b73a4f16ef263f173ce4c2690
vismaya2002 <vismay2002@gmail.com>
2023-10-02 20:04:40 +0530

    added polygon.py in python file

---
+++ python/polygon.py
@@ -0,0 +1,13 @@
+class Polygon:
+    #add code here
+    pass
+
+
+
+
+class Rectangle(Polygon):
+    #add code here
+
+    pass
+
+#similarly create class for square, triangle and quadrilateral which is inherited from base class Polygon.
