ea5c25f72b1191c56a2853bc09a5abac8a95cde3
Arun <aryahrmth@gmail.com>
2023-10-02 13:46:44 +0530

    fix logic of multithread

--- java/multithread.java
+++ java/multithread.java
@@ -1,63 +1,68 @@
+//Code by Arun Hiremath
 import java.util.Random;
 import java.util.Scanner;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
-class Square implements Runnable {
-    int n;
+class Square implements Callable<Integer> {
+    private final int n;
 
-    Square(int n) {
+    public Square(int n) {
         this.n = n;
     }
 
-    public void run() {
-        int s = this.n * this.n;
-        System.out.println("Square of " + this.n + " = " + s);
+    @Override
+    public Integer call() {
+        return n * n;
     }
 }
 
-class Cube implements Runnable {
-    int n;
+class Cube implements Callable<Integer> {
+    private final int n;
 
-    Cube(int n) {
+    public Cube(int n) {
         this.n = n;
     }
 
-    public void run() {
-        int c = this.n * this.n * this.n;
-        System.out.println("Cube of " + this.n + " = " + c);
+    @Override
+    public Integer call() {
+        return n * n * n;
     }
 }
 
 public class Multithread {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("How Many Random Integers needed : ");
+        System.out.print("How Many Random Integers needed: ");
         int x = sc.nextInt();
-        System.out.print("Set the limit up to which random numbers should be generated : ");
+        System.out.print("Set the limit up to which random numbers should be generated: ");
         int l = sc.nextInt();
         sc.close();
 
         ExecutorService executor = Executors.newFixedThreadPool(x);
+        CompletionService<Integer> completionService = new ExecutorCompletionService<>(executor);
 
         for (int i = 10; i < 121; i++) {
             Random random = new Random();
             int ranInt = random.nextInt(l);
             System.out.println("The random integer: " + ranInt);
 
-            if (ranInt % 2 == 0) {
-                executor.submit(new Square(ranInt));
-            } else {
-                executor.submit(new Cube(ranInt));
-            }
-
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                System.err.println(e);
-            }
+            Callable<Integer> task = (ranInt % 2 == 0) ? new Square(ranInt) : new Cube(ranInt);
+            completionService.submit(task);
         }
 
         executor.shutdown();
+
+        try {
+            for (int i = 0; i < 111; i++) {
+                Future<Integer> future = completionService.take();
+                int result = future.get();
+                System.out.println("Result: " + result);
+
+                // Sleep if needed
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            System.err.println(e);
+        }
     }
 }
