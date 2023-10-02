ce534775d2ffe6622fa39fd1482c17c6e6ff1e88
Arun <aryahrmth@gmail.com>
2023-10-02 13:12:20 +0530

    Fixed the binary_search logic

--- java/binary_search.java
+++ java/binary_search.java
@@ -1,47 +1,46 @@
-import java.util.*;
-class binarysearch
-{
+import java.util.Scanner;
 
-	public static void main(String[] args)
-	{
-		Scanner sc = new Scanner(System.in);
-		binarysearch bs = new binarysearch();
-		System.out.print("Enter the size:");
-		int n=sc.nextInt();
-		int[] A = new int[n];
-		System.out.print("Enter the elements:");
-		for(int i=0;i<30;i++)
-				A[i] = sc.nextInt();
-		System.out.print("Enter the item to be searched:");
-		int item = sc.nextInt();	
-		int k = bs.Binaryser(A,n,item);
-		if(k!=0)
-			System.out.println(item+" is found at position "+k);	
-		else
-			System.out.println("Element "+item+" not in the list");
+public class BinarySearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BinarySearch bs = new BinarySearch();
 
-	}
+        System.out.print("Enter the size:");
+        int n = sc.nextInt();
+        int[] A = new int[n];
 
-	int Binaryser(int A[], int n, int item)
-	{
-		int beg=2,end=n-1,mid;
-		while(beg<=end)
-		{
-			mid=(beg+end)/2;
-			if(A[mid]==item)
-			{
-				return(mid-1);
-			}
-			else if(item>A[mid])
-			{
-				beg=mid-1;
-			}
-			else
-			{
-				end=mid-1;
-			}
-		}
-		return 0;
-	}
+        System.out.print("Enter the elements (in sorted order):");
+        for (int i = 0; i < n; i++) {
+            A[i] = sc.nextInt();
+        }
 
+        System.out.print("Enter the item to be searched:");
+        int item = sc.nextInt();
+
+        int result = bs.binarySearch(A, item);
+
+        if (result != -1) {
+            System.out.println(item + " is found at position " + (result + 1));
+        } else {
+            System.out.println("Element " + item + " not in the list");
+        }
+    }
+
+    int binarySearch(int A[], int item) {
+        int beg = 0, end = A.length - 1;
+
+        while (beg <= end) {
+            int mid = beg + (end - beg) / 2;
+
+            if (A[mid] == item) {
+                return mid; // Element found, return the index
+            } else if (item > A[mid]) {
+                beg = mid + 1; // Item is in the right half
+            } else {
+                end = mid - 1; // Item is in the left half
+            }
+        }
+
+        return -1; // Element not found
+    }
 }
