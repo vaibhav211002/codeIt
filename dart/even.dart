ed231b653dc75daa2f8ad156ad1579ce2b10918f
Prabhat Pundeer <prabhat12pundeer@gmail.com>
2023-10-03 01:11:23 +0530

    new list containing only even nums

--- dart/even.dart
+++ dart/even.dart
@@ -1,3 +1,12 @@
 void main() {
   List<int> a = [1, 4, 34, 99, 21, 45, 66, 2, 25, 68, 77, 16, 25, 36, 49, 64, 81, 100];
+  List<int> evenElements = [];
+
+  for (int number in a) {
+    if (number % 2 == 0) {
+      evenElements.add(number);
+    }
+  }
+
+  
 }
